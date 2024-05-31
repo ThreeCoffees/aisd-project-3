28893f68019b1b66bd1f24387d80505fbcfdc3c7
ThreeCoffees <leonard.madzielewski@gmail.com>
2024-05-31 13:52:34 +0200

    c4 untested

--- main.cpp
+++ main.cpp
@@ -36,17 +36,15 @@ void cycle_detection_dfs(long long * c4_count, graph_t * graph, unsigned int cur
         for(unsigned int i = 0; i < graph->vertices[curr].n_count; i++){
             unsigned int curr_neighbor = graph->vertices[curr].neighbors[i];
             if(curr_depth == MAX_DFS_DEPTH && curr_neighbor == first_vert){
-                c4_count++;
+                (*c4_count)++;
             }
             if(visited[curr_neighbor] == true) continue;
 
             cycle_detection_dfs(c4_count, graph, curr_neighbor, visited, curr_depth+1, first_vert);
         }
     }
-    else{
-        visited[curr] = 0;
-    }
 
+    visited[curr] = 0;
 }
 
 void number_of_c4_subgraphs(graph_t * graph){
@@ -55,6 +53,8 @@ void number_of_c4_subgraphs(graph_t * graph){
     for(unsigned int i = 0; i < graph->v_count; i++){
         cycle_detection_dfs(&c4_count, graph, i, visited, 1, i);
     }
+
+    c4_count /= 8;
 
     free(visited);
     printf("%lld\n", c4_count);
