03c75b080eaf2eef94b81fc9df57344a494af607
ThreeCoffees <leonard.madzielewski@gmail.com>
2024-05-31 13:47:43 +0200

    eccentricity final + bipartitness untested

--- main.cpp
+++ main.cpp
@@ -27,14 +27,43 @@ void vertices_colors(graph_t * graph){
     printf("?\n");
 }
 
+#define MAX_DFS_DEPTH 4
+
+void cycle_detection_dfs(long long * c4_count, graph_t * graph, unsigned int curr, bool * visited, int curr_depth, unsigned int first_vert){
+    visited[curr] = 1;
+
+    if(curr_depth <= MAX_DFS_DEPTH){
+        for(unsigned int i = 0; i < graph->vertices[curr].n_count; i++){
+            unsigned int curr_neighbor = graph->vertices[curr].neighbors[i];
+            if(curr_depth == MAX_DFS_DEPTH && curr_neighbor == first_vert){
+                c4_count++;
+            }
+            if(visited[curr_neighbor] == true) continue;
+
+            cycle_detection_dfs(c4_count, graph, curr_neighbor, visited, curr_depth+1, first_vert);
+        }
+    }
+    else{
+        visited[curr] = 0;
+    }
+
+}
+
 void number_of_c4_subgraphs(graph_t * graph){
-    printf("?\n");
+    bool * visited = (bool *) calloc(graph->v_count, sizeof(bool));
+    long long c4_count = 0;
+    for(unsigned int i = 0; i < graph->v_count; i++){
+        cycle_detection_dfs(&c4_count, graph, i, visited, 1, i);
+    }
+
+    free(visited);
+    printf("%lld\n", c4_count);
 }
 
 void print_graph_info(graph_t * graph){
     degree_sequence(graph);
     number_of_components(graph);        
-    bipartiteness(graph);               //TODO
+    bipartiteness(graph);               
     eccentricity_of_vertices(graph);    
     planarity(graph);                   //TODO
     vertices_colors(graph);             //TODO
